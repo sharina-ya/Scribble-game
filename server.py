1967105ce4d2d4e49e1b0cad6e087e1cf4dbe1eb
sharina-ya <uliasarina7@gmail.com>
2024-10-09 04:28:47 +1000

    server.py

--- server.py
+++ server.py
@@ -1,85 +1,95 @@
+import sys
 import socket
 from _thread import *
-import sys
+from PyQt5 import QtWidgets, QtCore
 
-# server = "192.168.41.16"
-server = "192.168.141.246"
+
 port = 5555
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 currentPlayer = 0
-
-try:
-    s.bind((server, port))
-except socket.error as e:
-    str(e)
-
-s.listen(2)
-print("Server started\nWaiting for connection")
+info = [(0, 0, "", "b"), (0, 0, "", "b")]
 
 
-# считывает координаты рисование
-def read_position(s):
-    s = s.split(",")
-    return (s[0]), (s[1])
-
-# преобразовывает координаты в формат
-def make_position(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-# позиции игроков по дефолту
-pos = [(0, 0),(100, 100)]
-
-# цикл работы клиентов
-# потоки и тд
-def threaded_client(conn, player):
+def start_server(ip):
     global currentPlayer
-    conn.send(str.encode(make_position(pos[player])))
-    reply = ""
+    try:
+        s.bind((ip, port))
+    except socket.error as e:
+        print(str(e))
+    s.listen(2)
+    print("Server started\nWaiting for connection")
 
     while True:
+        conn, addr = s.accept()
+        print("Connected to:", addr)
+        start_new_thread(threaded_client, (conn, currentPlayer))
+        currentPlayer += 1
+
+
+def read_info(s):
+    s = s.split(",")
+    return (s[0]), (s[1]), (s[2]), (s[3])
+
+
+def make_info(tup):
+    return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2]) + "," + str(tup[3])
+
+
+def threaded_client(conn, player):
+    global currentPlayer
+    conn.send(str.encode(make_info(info[player])))
+    reply = ""
+    while True:
         try:
-
-
-
-            # считывание координат в виду стоки
-            data = read_position(conn.recv(2024).decode())
-            pos[player] = data
-            # обновление координат
-
-
+            data = read_info(conn.recv(2024).decode())
+            info[player] = data
             if not data:
-                # не получаем инфы
                 print("Disconnected")
+                info[player] = (0, 0, "", "b")
                 break
             else:
-                # определение от кого получили инфу о координатах
                 if player == 1:
-                    # отправка от клиента 2
-                    reply = pos[0]
+                    info[0] = (info[0][0], info[0][1], data[2], info[0][3])
+                    reply = info[0]
                 else:
-                    reply = pos[1]
-                # получил отправил для ориентации
+                    reply = info[1]
                 print("Received: ", data)
                 print("Sending: ", reply)
-            # кодировка ???
-            conn.sendall(str.encode(make_position(reply)))
-
-
+            conn.sendall(str.encode(make_info(reply)))
         except:
             break
-    # при прырывании цикла отключается один из клиентов
     currentPlayer -= 1
-    # потеря соединение и закрытие
     print("Lost connection")
     conn.close()
 
-while True:
-    # принимает подключение клиента s - soket
-    conn, addr = s.accept()
-    print("Connected to:", addr)
 
-    # новый поток
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+class ServerApp(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Сервер')
+        self.setGeometry(100, 100, 300, 150)
+
+        self.setStyleSheet("background-color: pink;")
+
+        self.ipInput = QtWidgets.QLineEdit(self)
+        self.ipInput.setPlaceholderText("Введите IP")
+        self.ipInput.setGeometry(50, 30, 200, 30)
+
+        self.startButton = QtWidgets.QPushButton('Запустить сервер', self)
+        self.startButton.setGeometry(50, 80, 200, 30)
+        self.startButton.clicked.connect(self.start_server)
+
+    def start_server(self):
+        ip = self.ipInput.text()
+        self.startButton.setEnabled(False)
+        start_new_thread(start_server, (ip,))
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = ServerApp()
+    ex.show()
+    sys.exit(app.exec_())
