51d79c5478d0d22d00f4f72a4706da8f56ca7a87
sharina-ya <uliasarina7@gmail.com>
2024-10-09 04:29:22 +1000

    network.py

--- network.py
+++ network.py
@@ -3,28 +3,28 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.server = "192.168.41.16"
+
         self.server = "192.168.141.246"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.info = self.connect()
 
-    def getPos(self):
-        return self.pos
+    def getInfo(self):
+        return self.info
 
-    # клиент запрашивает подключении к серверу и считывает инфу от сервера
+    # client connets to server and receive info from server
     def connect(self):
         try:
+
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
         except:
             pass
 
+    # client sends info to server and receive data from server
     def send(self, data):
         try:
             self.client.send(str.encode(data))
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
-
-
