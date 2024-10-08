ab4bc5ff0fa25544dc882847292943e9a36bcfb0
G-A223 <gunko.anastassiya@yandex.ru>
2024-10-09 01:45:59 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import socket
 from _thread import *
-import sys
 
 server = "192.168.41.16"
 #server = "192.168.141.246"
 port = 5555
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 currentPlayer = 0
+
+#start info abaout players
+#начальная информация об игроках
+info = [(0, 0, "", "b"), (0, 0, "", "b")]
 
 try:
     s.bind((server, port))
@@ -19,69 +20,63 @@ s.listen(2)
 print("Server started\nWaiting for connection")
 
 
-# считывает координаты рисование
-def read_position(s):
+#converts recieved data into tuple
+#преобразовывает полученную информацию в кортеж
+def read_info(s):
     s = s.split(",")
     return (s[0]), (s[1]), (s[2]), (s[3])
 
-# преобразовывает координаты в формат
-def make_position(tup):
+#converts data into string (to send it to another server)
+#преобразовывает нужную информацию в строку, которую потом отправим на другой сервер
+def make_info(tup):
     return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2]) + "," + str(tup[3])
 
-# позиции игроков по дефолту
-pos = [(0, 0, "", "b"),(100, 100, "", "b")]
-
-# цикл работы клиентов
-# потоки и тд
+#working with clients info
+#работаем с информацией клиентов
 def threaded_client(conn, player):
     global currentPlayer
-    conn.send(str.encode(make_position(pos[player])))
+    conn.send(str.encode(make_info(info[player])))
     reply = ""
 
     while True:
         try:
-
-
-
-            # считывание координат в виду стоки
-            data = read_position(conn.recv(2024).decode())
-            pos[player] = data
-            # обновление координат
-
+            #recieving data from client and update it's information
+            #принимаем информацию от клиента и обновляем ее
+            data = read_info(conn.recv(2024).decode())
+            info[player] = data
 
             if not data:
-                # не получаем инфы
                 print("Disconnected")
-                pos[player] = (0, 0, "", "b")
+                info[player] = (0, 0, "", "b")
                 break
             else:
-                # определение от кого получили инфу о координатах
+                #если инфо получено от того, кто угадывает
+                #if info was received from the one who guessing
                 if player == 1:
-                    pos[0] = (pos[0][0], pos[0][1], data[2], pos[0][3])
-                    # отправка от клиента 2
-                    reply = pos[0]
+                    info[0] = (info[0][0], info[0][1], data[2], info[0][3])
+                    reply = info[0]
                 else:
-                    reply = pos[1]
-                # получил отправил для ориентации
+                    reply = info[1]
+                    
                 print("Received: ", data)
                 print("Sending: ", reply)
-            # кодировка ???
-            conn.sendall(str.encode(make_position(reply)))
-
-
+            
+            conn.sendall(str.encode(make_info(reply)))
+            
         except:
             break
-    # при прырывании цикла отключается один из клиентов
+
     currentPlayer -= 1
-    # потеря соединение и закрытие
     print("Lost connection")
     conn.close()
 
 while True:
-    # принимает подключение клиента s - soket
+    #getting new connection from client
+    #принимает подключение клиента
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    # новый поток
+    #starting new thread
+    #создаем новый поток
     start_new_thread(threaded_client, (conn, currentPlayer))
     currentPlayer += 1
